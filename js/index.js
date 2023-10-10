aacd0e73a162359d6588b68b1c0d132293cb0bcd
Joinside <johannespopp2006@protonmail.com>
2023-10-10 23:03:03 +0200

    Startseite funktioniert

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
 let lang = localStorage.getItem('lang') || 'de';
 
-import { languageText } from './lang.js';
-// Texte für verschiedene Sprachen
+const languageText = {
+    de: {
+        headingStart: "Europareise",
+        playerAmountText: "Spieleranzahl auswählen: ",
+        checkButton: "Spiel starten",
+        choosePlayerAmountMessage: "Wählen Sie aus",
+        errorMessagePlayerAmount: "Bitte wählen Sie eine gültige Spieleranzahl aus. Mind. 2",
+        errorMessagePlayerNames: "Bitte füllen Sie alle Spielerfelder aus.",
+        player: "Spieler",
+        loadGameText: "Spiel aus Speicher laden",
+        loadGameButton: "Spiel laden",
+    },
+    en: {
+        headingStart: "Journey through Europe",
+        playerAmountText: "Choose number of players: ",
+        checkButton: "Start Game",
+        choosePlayerAmountMessage: "Please choose",
+        errorMessagePlayerAmount: "Please choose a valid number of players. Min. 2",
+        errorMessagePlayerNames: "Please fill out all player fields.",
+        player: "Player",
+        loadGameText: "Load game from storage",
+        loadGameButton: "Load game",
+    }
+};
 
-const playerNameCheck = {}; // Object to track whether player names are filled
+const playerNameCheck = {};
 
-// Funktion zum Ändern der Sprache und Aktualisieren des angezeigten Textes
 function changeLanguage() {
     document.getElementById('headingStart').textContent = languageText[lang].headingStart;
     document.getElementById('playerAmountText').textContent = languageText[lang].playerAmountText;
-    document.getElementById('checkButton').textContent = languageText[lang].checkButton;
+    document.getElementById('checkButton').value = languageText[lang].checkButton;
     document.getElementById('choosePlayerAmountMessage').textContent = languageText[lang].choosePlayerAmountMessage;
-    // Weitere Textelemente hier aktualisieren, wenn erforderlich
-    // Check if tbody is not empty and change placeholder
+
     const tbody = document.getElementById('playerNamesTbody');
     if (tbody.innerHTML !== '') {
         const playerAmount = parseInt(playerAmountSelect.value);
@@ -25,44 +45,60 @@ function changeLanguage() {
     }
 }
 
-// Event-Listener für Änderungen der Sprachauswahl
 const langSelectElement = document.getElementById('langSelect');
 
 langSelectElement.addEventListener('change', langChange);
 
 function langChange() {
-    const choosenLang = langSelectElement.value;
-    if ((choosenLang !== lang) && languageText[choosenLang]) {
-        lang = choosenLang;
+    const chosenLang = langSelectElement.value;
+    if ((chosenLang !== lang) && languageText[chosenLang]) {
+        lang = chosenLang;
         changeLanguage();
         localStorage.setItem('lang', lang);
-        console.log("Sprache geändert zu " + lang);
+        console.log("Sprache geändert zu " + chosenLang);
     } else {
         console.log('Bereits ausgewählt oder nicht vorhanden');
+    }
+    const savedGame = localStorage.getItem('game');
+    if (savedGame !== null) {
+        showLoadGameButton();
     }
 }
 
 window.addEventListener('load', () => {
-    // Versuche, die Sprache aus dem Local Storage abzurufen
     const savedLang = localStorage.getItem('lang');
-
-    // Wenn eine gespeicherte Sprache gefunden wurde und sie in der Liste der unterstützten Sprachen ist, setze sie als aktuelle Sprache
-    if (savedLang !== '' && languageText[savedLang]) {
+    if (savedLang !== null && languageText[savedLang]) {
         lang = savedLang;
     }
-
-    // Aktualisiere die Sprachauswahl im Dropdown
     langSelectElement.value = savedLang;
-    // Ändere die Sprache auf der Webseite
     langChange();
 });
+
+function showLoadGameButton() {
+    const loadGameForm = document.getElementById('loadGameForm');
+    const loadGameButton = document.getElementById('loadGameButton');
+    loadGameForm.style.display = 'block';
+    loadGameButton.addEventListener('click', redirectToLoadGame);
+}
+
+function redirectToLoadGame() {
+    window.location.href = "game_load.html";
+}
+
+const removeGame = document.getElementById('removeGame');
+removeGame.addEventListener('click', removeGameFromStorage);
+
+function removeGameFromStorage() {
+    localStorage.removeItem('game');
+    const loadGameForm = document.getElementById('loadGameForm');
+    loadGameForm.style.display = 'none';
+}
 
 const playerAmountSelect = document.getElementById('playerAmountSelect');
 playerAmountSelect.addEventListener('change', playerNamesInputsChange);
 
 function playerNamesInputsChange() {
     const playerAmount = parseInt(playerAmountSelect.value);
-
     const tbody = document.getElementById('playerNamesTbody');
     tbody.innerHTML = '';
 
@@ -85,42 +121,39 @@ function playerNamesInputsChange() {
     }
 }
 
-
 function playerNamesInputChecker(index) {
     const input = document.getElementById('playerName' + index);
     if (input.value.trim() !== '') {
         playerNameCheck[index] = true;
         localStorage.setItem('playerName' + index, input.value.trim());
-    }else{
+    } else {
         playerNameCheck[index] = false;
         localStorage.setItem('playerName' + index, '');
     }
 }
 
-// Add action listener on form submit
-const form = document.getElementById('playerNamesForm');
+const form = document.getElementById('playerAmountSelectForm'); // Changed the form id to 'playerAmountSelectForm'
 form.addEventListener('submit', checkForm);
 
 const errorMessage = document.getElementById('errorMessage');
 
-function checkForm(event){
+function checkForm(event) {
     event.preventDefault();
-
-    const playerAmount = playerAmountSelect.value;
-    errorMessage.textContent = '';
-    if(playerAmount === "0"){
+    const playerAmount = parseInt(playerAmountSelect.value);
+    if (playerAmount === 0) {
         errorMessage.textContent = languageText[lang].errorMessagePlayerAmount;
-    }else{
-        for(let i = 1; i <= playerAmount; i++){
-            const input = document.getElementById('playerName' + i);
-            let inputvalue = input.value.trim();
-
-            if(inputvalue === ''){
-                errorMessage.textContent = languageText[lang].errorMessagePlayerNames;
+    } else {
+        let allPlayersFilled = true;
+        for (let i = 1; i <= playerAmount; i++) {
+            if (!playerNameCheck[i]) {
+                allPlayersFilled = false;
                 break;
             }
         }
-        if(errorMessage.textContent === ''){
+        if (!allPlayersFilled) {
+            errorMessage.textContent = languageText[lang].errorMessagePlayerNames;
+        } else {
+            errorMessage.textContent = '';
             window.location.href = "game_start.html";
         }
     }
